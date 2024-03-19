7403de16f265c2680be816778710265fe3ee9b0f
meilin.huang <954537473@qq.com>
2024-03-19 17:31:10 +0800

    feat: 完善castd

--- castd.go
+++ castd.go
@@ -8,6 +8,22 @@ func ToIntD(i any, defaultValue int) int {
 	}
 }
 
+func ToUIntD(i any, defaultValue uint) uint {
+	if val := ToUint(i); val == 0 {
+		return defaultValue
+	} else {
+		return val
+	}
+}
+
+func ToInt64D(i any, defaultValue int64) int64 {
+	if val := ToInt64(i); val == 0 {
+		return defaultValue
+	} else {
+		return val
+	}
+}
+
 func ToUInt64D(i any, defaultValue uint64) uint64 {
 	if val := ToUint64(i); val == 0 {
 		return defaultValue
@@ -18,6 +34,22 @@ func ToUInt64D(i any, defaultValue uint64) uint64 {
 
 func ToInt8D(i any, defaultValue int8) int8 {
 	if val := ToInt8(i); val == 0 {
+		return defaultValue
+	} else {
+		return val
+	}
+}
+
+func ToUInt8D(i any, defaultValue uint8) uint8 {
+	if val := ToUint8(i); val == 0 {
+		return defaultValue
+	} else {
+		return val
+	}
+}
+
+func ToStringD(i any, defaultValue string) string {
+	if val := ToString(i); val == "" {
 		return defaultValue
 	} else {
 		return val
